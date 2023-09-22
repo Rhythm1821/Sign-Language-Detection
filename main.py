9f74d3a5072bf8fd74e18d4f0833be0b747f8411
Rhythm1821 <alamrawat1821@gmail.com>
2023-09-22 11:29:37 +0530

    main.py

--- main.py
+++ main.py
@@ -1,70 +1,30 @@
-import cv2
-from utils import *
-from keras.models import load_model
-import numpy as np
+from flask import Flask,render_template,request
+import sys, subprocess
+sys.path.append(".")
+from src.pipeline.training_pipeline import train_model
+from src.pipeline.prediction_pipeline import webcamdetection
 
+app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-# Detection variables
-sequence = []
-sentence = []
-predictions = []
-threshold = 0.4
-res = [0.7,0.2,0.1]
+@app.route("/train",methods=["POST"])
+def training():
+    if request.method=="POST":
+        train_model()
+    return render_template("training.html")
 
-model = load_model("model/signdetection.h5")
+@app.route("/test",methods=["POST"])
+def predicting():
+    if request.method == 'POST':
+        try:
+            # Run your webcam.py script as a subprocess
+            subprocess.Popen(['python', 'webcam.py'])
+            return "<h1> A webcam will appear and predict the sign. Press 'q' to quit</h1>" 
+        except Exception as e:
+            return f"Error: {str(e)}"
 
-colors = [(245,117,16),(117,245,16),(16,117,245)]
-actions = np.array(["hello","thanks","iloveyou"])
-
-cap = cv2.VideoCapture(0)
-
-with mp_holistic.Holistic(min_tracking_confidence=0.5,min_detection_confidence=0.5) as holistic:
-    while cap.isOpened():
-        # Read frame
-        ret,frame = cap.read()
-
-        # Make detections
-        image,results = mediapipe_detection(frame,holistic)
-
-        # Draw landmarks
-        draw_landmarks(image,results)
-
-        # Prediction logic
-        keypoints = extract_keypoints(results)
-        sequence.append(keypoints)
-        sequence = sequence[-30:]
-
-        if len(sequence)==30:
-            res = model.predict(np.expand_dims(sequence,axis=0))[0]
-            print(actions[np.argmax(res)])
-            predictions.append(np.argmax(res))
-
-            if np.unique(predictions[-10:])[0]==np.argmax(res):
-                if res[np.argmax(res)] > threshold:
-                    if len(sentence)>0:
-                        if actions[np.argmax(res)] != sentence[-1]:
-                            sentence.append(actions[np.argmax(res)])
-                    else:
-                        sentence.append(actions[np.argmax(res)])
-
-
-        cv2.rectangle(image,(0,0),(640,40),(245,117,16),-1)
-        cv2.putText(image," ".join(sentence),(3,30),
-                    cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255),2,cv2.LINE_AA)
-        
-        if len(sentence)>5:
-            sentence=sentence[-5:]
-        
-        # visualizing prob
-        image = prob_viz(res,actions,image,colors)
-
-        # Show to screen
-        cv2.imshow("Opencv feed",image)
-
-        # Break
-        if cv2.waitKey(10) & 0xFF==ord('q'):
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
+if __name__=="__main__":
+    app.run(debug=True)
